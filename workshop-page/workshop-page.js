4500d18113c0558848f59a65ddc62377266f9bbe
David <Rigby335@hotmail.com>
2022-05-24 11:41:17 -0700

    rewrote displayParticipants() and added create button

--- workshop-page/workshop-page.js
+++ workshop-page/workshop-page.js
@@ -2,17 +2,22 @@ import { checkAuth, logout, getWorkshops, deleteParticipant } from '../fetch-uti
 
 checkAuth();
 
-const logoutButton = document.getElementById('logout');
+const logoutButton = document.getElementById('logout-button');
 const workshopsEl = document.querySelector('.workshops-container');
+const createButton = document.getElementById('create-button');
 
 logoutButton.addEventListener('click', () => {
     logout();
 });
 
+createButton.addEventListener('click', () => {
+    window.location.href = '../create.page';
+});
+
 async function displayParticipants() {
     //fetch workshops from supabase
     const workshops = await getWorkshops();
-    // clear out the workshopEl
+    // clear out the workshopEl 
     workshopsEl.textContent = '';
     //create three elements for each workshop, one for the workshop, one to hold the name, and one to hold the bunnies
     for (let workshop of workshops) {
@@ -25,10 +30,10 @@ async function displayParticipants() {
         const h3 = document.createElement('h3');
         h3.textContent = workshop.name;
         // <div class="participants">
-            // <div class="participant"></div>
-            // <div class="participant"></div>
+        // <div class="participant"></div>
+        // <div class="participant"></div>
         const participantsDiv = document.createElement('div');
-        participantsDiv.classList.add('participants');        
+        participantsDiv.classList.add('participants');
         // add the participants css class to the participants el, and the workshop css class to the workshop el
         //  </div>
         //</div>
@@ -39,18 +44,17 @@ async function displayParticipants() {
             participantDiv.classList.add('participant');
             participantDiv.textContent = participant.name;
             // add an event listener to the participant el. On click, delete the participant, then refetch and redisplay all workshops
-            participantDiv.add('click', async () => {
+            participantDiv.addEventListener('click', async () => {
                 await deleteParticipant();
                 displayParticipants();
             });
             // append this participantEl to the participantsEl
-            participantDiv.append(participantsDiv);
+            participantsDiv.append(participantDiv);
         }
         // append the participantsEl and the nameEl to the workshopEl
-        // append the workshopEl to the workshopsEl
         workshopDiv.append(participantsDiv, h3);
+        // append the workshopEl to the workshopsEl
         workshopsEl.append(workshopDiv);
     }
 }
-
 displayParticipants();
